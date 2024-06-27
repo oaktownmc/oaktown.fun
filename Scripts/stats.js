00763aecf7a8c29b6be3fa783fc574487ec6c4e9
CallMePol <poleve540@gmail.com>
2024-06-26 20:16:10 -0400

    skibidi rizz

---
+++ Scripts/stats.js
@@ -0,0 +1,111 @@
+﻿var MentulaStats = function MentulaStats(callback) {
+    var _this = this;
+
+    //SET THE HOST TO THE SERVER IP.
+    this.host = "66.85.77.238";
+    this.port = "9922";
+    this.baseURI = `http://${this.host}:${this.port}`;
+    this.connectionURI = `${this.baseURI}/signalr`;
+    this.hubURI = `${this.baseURI}/signalr/hubs`;
+    this.resultsCallback = callback;
+    var hubsScript = document.createElement('script');
+    hubsScript.setAttribute("src", this.hubURI);
+    hubsScript.setAttribute("async", "false");
+    hubsScript.onload = function() {
+        $.connection.hub.url = _this.connectionURI;
+        _this.serverConnection = $.connection.ServerHub;
+        _this.initEvents();
+
+        $.connection.hub.start().done(function () {
+        });
+    }
+    document.head.append(hubsScript);
+}
+
+MentulaStats.prototype.initEvents = function () {
+    var _this = this;
+    this.serverConnection.client.GetStats = function(result) {
+        _this.GetStatsCallback(result);
+    }
+}
+
+MentulaStats.prototype.GetStats = function() {
+    this.serverConnection.server.getStats();
+}
+MentulaStats.prototype.GetStatsCallback = function(result) {
+    if (this.resultsCallback != null && this.resultsCallback != undefined)
+        this.resultsCallback(JSON.parse(result));
+}
+
+if (!Object.prototype.forEach) {
+    Object.defineProperty(Object.prototype, 'forEach', {
+        value: function (callback, thisArg) {
+            if (this == null) {
+                throw new TypeError('Not an object');
+            }
+            thisArg = thisArg || window;
+            for (var key in this) {
+                if (this.hasOwnProperty(key)) {
+                    callback.call(thisArg, this[key], key, this);
+                }
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
+
+var MentulaExtra = function Mentula() {
+    this.mapStrings = {
+        ascension: "Ascension",
+        backwash: "Backwash",
+        beavercreek: "Beaver Creek",
+        burial_mounds: "Burial Mounds",
+        coagulation: "Coagulation",
+        colossus: "Colossus",
+        containment: "Containment",
+        cyclotron: "Ivory Tower",
+        deltatap: "Sanctuary",
+        dune: "Relic",
+        elongation: "Elongation",
+        foundation: "Foundation",
+        gemini: "Gemini",
+        headlong: "Headlong",
+        lockout: "Lockout",
+        midship: "Midship",
+        street_sweeper: "District",
+        terminal: "Terminal",
+        triplicate: "Uplift",
+        needle: "Uplift",
+        turf: "Turf",
+        warlock: "Warlock",
+        waterworks: "Waterworks",
+        zanzibar: "Zanzibar"
+    };
+    this.statusStrings = {
+        Lobby: "In Lobby",
+        Starting: "Game Starting",
+        InGame: "Playing",
+        PostGame: "Post Game",
+        MatchMaking: "Finding Players",
+        Unknown: "Captain There is a problem"
+    };
+}
+
+MentulaExtra.prototype.localizeMapName = function (mapName) {
+    //Custom Map supportish...
+    if (mapName === "")
+        return "N/A";
+    var name = this.mapStrings[mapName];
+    if (name === undefined)
+        name = mapName.char(0).toUpperCase() + mapName.slice(1);
+    return name;
+}
+MentulaExtra.prototype.localizeStatus = function (status) {
+    return this.statusStrings[status];
+}
---
+++ hellobro/script.js
@@ -0,0 +1,87 @@
+const servers = [
+    // OAK servers :D
+    ["158.101.122.160:27459", "hl2dm"],
+    ["158.101.122.160:27459", "hl2dm"],
+    ["158.101.122.160:21171", "minecraft"],
+    ["158.101.122.160:8123", "hl2dm"],
+    ["150.136.164.5:27015", "hl2dm"],
+    ["158.101.122.160:7200", "minecraft"],
+    ["158.101.122.160:17652", "minecraft"]
+  ];
+
+function fetchServerData(server) {
+    const serverAddress = server[0]
+    const serverGame = server[1]
+    const url = `https://api.raccoonlagoon.com/v1/server-info?ip=${serverAddress}&g=${serverGame}`;
+    serverElement = document.createElement("div")
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayServerData(data, server[1]))
+        .catch(error => console.error("Error fetching server data:", error));
+}
+
+const SECONDS_IN_DAY = 24 * 60 * 60;
+const SECONDS_IN_HOUR = 60 * 60;
+const SECONDS_IN_MINUTE = 60;
+
+function convertTime(time) {
+    const days = Math.floor(time / SECONDS_IN_DAY);
+    const hours = Math.floor((time % SECONDS_IN_DAY) / SECONDS_IN_HOUR);
+    const minutes = Math.floor((time % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
+    const seconds = Math.floor(time % SECONDS_IN_MINUTE);
+
+    let formattedTime = "";
+
+    // only show applicable time units
+    if (days > 0) formattedTime += `${days}d `;
+    if (hours > 0) formattedTime += `${hours}h `;
+    if (minutes > 0 || hours > 0) formattedTime += `${minutes}m `;
+
+    formattedTime += `${seconds.toString()}s`; // add seconds
+    return formattedTime; // return formatted time string
+}
+
+function displayServerData(data, game) {
+
+    const container = document.getElementById("servers");
+    const serverElement = document.createElement("div");
+
+    const cleanServerName = data.serverName?.replace(/�./g, "");
+
+    const numOfBots = data.numBots > 0 ? ` (${data.numBots} bots)` : ""; // only show bots if there are any
+
+    // only show the table if there are players
+    const playerListTable = data.humanData.length > 0 ? `
+    <table>
+    <tr>
+        <th>Name</th>
+        <th>Score</th>
+        <th>Time Played</th>
+    </tr>${data.humanData.map(player => {
+        return `<tr><td>${player.name}</td><td>${player.score}</td><td>${convertTime(player.time)}</td></tr>`;
+    }).join("")}` : "";
+
+    // only show button if server isnt full
+    const connectButton = document.createElement("button");
+    connectButton.innerHTML = "Connect";
+    connectButton.onclick = () => {
+        window.open(`steam://connect/${data.serverIP}`);
+    };
+
+    serverElement.innerHTML = `
+        <h6>${game.toUpperCase()} SERVER</h6>
+        <h2>${cleanServerName}</h2>
+        <p><b>IP:</b> ${data.serverIP}</p>
+        <p><b>Current Map:</b> ${data.currentMap}</p>
+        <p><b>Players:</b> ${data.numHumans}/${data.maxClients} ${numOfBots}</p>
+        ${playerListTable}
+    `;
+
+    if (data.numHumans < data.maxClients) {
+        serverElement.appendChild(connectButton);
+    }
+
+    container.appendChild(serverElement);
+}
+
+servers.forEach(server => fetchServerData(server));
